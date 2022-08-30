4a1cb41508ea7889368f41f7b0b35331db1367e0
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2022-08-29 21:34:20 -0300

    Projeto reniciado, com api ScaleApi funcionando na tela filtros

--- Telas/FiltrosTela.js
+++ Telas/FiltrosTela.js
@@ -2,10 +2,37 @@ import * as React from 'react';
 import {SafeAreaView , Button, Image, StyleSheet, TouchableOpacity, Text, View, ImageBackground } from 'react-native';
 import Cores from '../Constantes/Cores';
 import styles from '../Constantes/Styles'
-
 const image = require('../Imagens/Fundo.png');
 
+const axios = require('axios');
+
+// set up the request parameters
+
+
 const FiltrosTela = ({navigation}) => {
+  const axios = require('axios');
+
+// set up the request parameters
+  const params = {
+    api_key: "B9E7BDF7D3024533B62B918CED851541",
+    location: "Brazil",
+    search_type: "shopping",
+    q: "5600g"
+  }
+
+  // make the http GET request to Scale SERP
+  axios.get('https://api.scaleserp.com/search', { params })
+    .then(response => {
+
+      // print the JSON response from Scale SERP
+      console.log(JSON.stringify(response.data.shopping_results[0].title, 0, 2));
+
+    }).catch(error => {
+      // catch and print the error
+      console.log(error);
+    });
+
+
   return (
     <SafeAreaView style={styles.tela}>
       <SafeAreaView style={styles.conteudo}>
