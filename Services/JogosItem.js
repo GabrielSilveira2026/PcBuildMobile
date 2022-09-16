ed0a36ca1255dc62b2d2c1a62ca67922a6355faf
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2022-09-15 22:31:44 -0300

    componentes buttons trocados por TouchbleOpacity com estilização melhorada

--- Services/JogosItem.js
+++ Services/JogosItem.js
@@ -8,9 +8,10 @@ import React from 'react'
 
 const jogosItem = ({jogo}) => {
     const name = jogo.name
-
+    const imagem = jogo.image
   return (
     <View style={styles.cartao}>
+        <Image style={styles.imagem} source={{uri: imagem}}/>
         <Text>Nome: {name}</Text>
     </View>
   )
@@ -23,10 +24,19 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         backgroundColor: "#ff7e75",
         padding: 10,
-        width: 250,
+        width: "85%",
         marginLeft:'auto',
         marginRight:'auto',
         borderRadius: 8,
         borderWidth:3
-    }
+    },
+    imagem:{
+        borderRadius:8,
+        borderWidth: 1,
+        borderColor:'black',
+        height: 150,
+        width: "100%",
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
 })
