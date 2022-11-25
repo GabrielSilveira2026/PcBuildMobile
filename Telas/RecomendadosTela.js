64785d501d116ab133940a849ca886b28986e46c
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2022-11-25 04:08:56 -0300

    Ajustes visuais realizados, retirado alguns logs

--- Telas/RecomendadosTela.js
+++ Telas/RecomendadosTela.js
@@ -15,18 +15,34 @@ const RecomendadosTela = ({route, navigation}) => {
   const [pcRecomendado, setPcRecomendado] = useState([])
   const reqs = extraiRequisitosDeUmaLista(selecionados.cart) // usar .listaRequisitos
 
-  /*
-    configuracoes[
-      {tipo:minimo, pecas[gpu:{}]},
-      {tipo:recomendado, pecas[gpu:{}]},
-    ]
-  */
   useEffect(()=>{
     async function montaPC(){
       // console.log(await calculaPlacaBackEnd(selecionados.cart, 'recomendados'));
       let gpu = await calculaPlacaBackEnd(selecionados.cart, 'minimos')
       gpu?
-          setPcMinimo([gpu])
+          setPcMinimo([
+            {
+              "title": "Processador Amd Ryzen 5 5600 3.5Ghz (4.4GHz Turbo) Am4",
+              "id": "14541321093222037555",
+              "link": "https://www.google.com/shopping/product/14541321093222037555",
+              "price_raw": "R$ 999,01",
+              "merchant": "Pichau",
+              "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR4qyvzS6QQ3TP_1bTwRKo9A5D-9PndIS9Jv0fQ1_u3hnIKBkDy7XnJLir1xq-V6O7zd9Zt1jJt0xQ_caF9oaqDzvsoodfUrsjFOIktED-0A3eIOQIyptPE&usqp=CAE",
+            },
+            gpu,
+            {
+              "title": "Memória Kingston 8gb Ddr4 3200mhz Fury Beast Rgb KF432C16BBA/8",
+              "id": "9586862228788569367",
+              "link": "https://www.google.com/shopping/product/9586862228788569367",
+              "rating": 5,
+              "reviews": 59,
+              "merchant_count": 10,
+              "price_raw": "R$ 209,99",
+              "merchant": "KaBuM!",
+              "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRd4hI8mxaRZrohEmSNW6FFyL4WU1VOJTXvgHCFC_Z_WyZ_1qTFZ2NhnMaf9fG-00wUUA8PNTcJWZ4pAs2VR0Fq97hcS1R5F0k1-M-Ju589hVIjcRM-AQ-_&usqp=CAE",
+              "position": 1,
+            }
+          ])
       :
         setCarrega(false)
 
@@ -34,7 +50,17 @@ const RecomendadosTela = ({route, navigation}) => {
       gpu = await calculaPlacaBackEnd(selecionados.cart, 'recomendados')
 
       gpu?
-        setPcRecomendado([gpu])
+        setPcRecomendado([
+          {
+            "title": "Processador Amd Ryzen 5 5600 3.5Ghz (4.4GHz Turbo) Am4",
+            "id": "14541321093222037555",
+            "link": "https://www.google.com/shopping/product/14541321093222037555",
+            "price_raw": "R$ 999,01",
+            "merchant": "Pichau",
+            "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR4qyvzS6QQ3TP_1bTwRKo9A5D-9PndIS9Jv0fQ1_u3hnIKBkDy7XnJLir1xq-V6O7zd9Zt1jJt0xQ_caF9oaqDzvsoodfUrsjFOIktED-0A3eIOQIyptPE&usqp=CAE",
+          },
+          gpu
+        ])
       :
         setCarrega(false)
       
@@ -46,53 +72,7 @@ const RecomendadosTela = ({route, navigation}) => {
       setPcMinimo()
     }
   },[])
-
-  // const pcRecomendado=[
-  //   {
-  //     "title": "Processador Amd Ryzen 5 5600 3.5Ghz (4.4GHz Turbo) Am4",
-  //     "id": "14541321093222037555",
-  //     "link": "https://www.google.com/shopping/product/14541321093222037555",
-  //     "price_raw": "R$ 999,01",
-  //     "merchant": "Pichau",
-  //     "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR4qyvzS6QQ3TP_1bTwRKo9A5D-9PndIS9Jv0fQ1_u3hnIKBkDy7XnJLir1xq-V6O7zd9Zt1jJt0xQ_caF9oaqDzvsoodfUrsjFOIktED-0A3eIOQIyptPE&usqp=CAE",
-  //   },
-  //   {
-  //     "title": "Placa De Vídeo Msi Geforce RTX 2060 6gb Ventus Gp Oc GDDR6 - 912-V375-808",
-  //     "id": "17368229848816949007",
-  //     "link": "https://www.google.com/shopping/product/17368229848816949007",
-  //     "price_raw": "R$ 1.659,98",
-  //     "merchant": "Pichau",
-  //     "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTdPZkMfH1nwiyaaFZcXEPWETDX3Oy6ChbsugPWsLYimCI5EkQ4zCWPHzLov86Wr6tSjP-nimlk9hgxNi5-qdOzWZlLEWjeo72TGVQH7NSfyc9KcM8Th6Y4hQ&usqp=CAE",
-  //   },
-  // ]
-  // const pcMinimo=[
-  //   {
-  //     "title": "Processador Amd Ryzen 3 5600 3.5Ghz (4.4GHz Turbo) Am4",
-  //     "id": "14541321093222037555",
-  //     "link": "https://www.google.com/shopping/product/14541321093222037555",
-  //     "price_raw": "R$ 999,01",
-  //     "merchant": "Pichau",
-  //     "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR4qyvzS6QQ3TP_1bTwRKo9A5D-9PndIS9Jv0fQ1_u3hnIKBkDy7XnJLir1xq-V6O7zd9Zt1jJt0xQ_caF9oaqDzvsoodfUrsjFOIktED-0A3eIOQIyptPE&usqp=CAE",
-  //   },
-  //   {
-  //     "title": "Placa De Vídeo Msi Geforce RTX 1060 6gb Ventus Gp Oc GDDR6 - 912-V375-808",
-  //     "id": "17368229848816949007",
-  //     "link": "https://www.google.com/shopping/product/17368229848816949007",
-  //     "price_raw": "R$ 1.659,98",
-  //     "merchant": "Pichau",
-  //     "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTdPZkMfH1nwiyaaFZcXEPWETDX3Oy6ChbsugPWsLYimCI5EkQ4zCWPHzLov86Wr6tSjP-nimlk9hgxNi5-qdOzWZlLEWjeo72TGVQH7NSfyc9KcM8Th6Y4hQ&usqp=CAE",
-  //   },
-  //   {
-  //     "title": "Memória Kingston 8gb Ddr4 3200mhz Fury Beast Rgb KF432C16BBA/8",
-  //     "id": "9586862228788569367",
-  //     "link": "https://www.google.com/shopping/product/9586862228788569367",
-  //     "price_raw": "R$ 243,00",
-  //     "merchant": "KaBuM!",
-  //     "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSaxY0kC6qgpcUGhn2Xdgz_y3ru9aDRundtlux7l_nJwWdlG_0pavvbQMsybotg9pkburOyf9AbgYZEjvOdg-zWb0_ogK3GlZ_CHGrsJeZNQbgj5tDkcyh5Hg&usqp=CAE",
-  //     "position": 1,
-  //     "has_product_page": true
-  //   },
-  // ]
+  
   return (
     <ImageBackground backgroundColor={Cores.secondary} source={imagemFundo} resizeMode="stretch" style={stylesGlobal.backgroundImage}>
       <View style={stylesGlobal.conteudoTela}>
