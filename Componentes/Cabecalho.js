fb4cfcf2c82c48b54c6cb625909f216aec250c25
gabriel20silver <gabriel20silver@hotmail.com>
2024-04-23 00:33:12 -0300

    Removido validaToken temporariamente

--- Componentes/Cabecalho.js
+++ Componentes/Cabecalho.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import styles, {Cores} from '../Constantes/Styles'
-import {useNavigation } from '@react-navigation/native';
-import {SafeAreaView, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
-import {FontAwesome5} from 'react-native-vector-icons';
+import styles, { Cores } from '../Constantes/Styles'
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { FontAwesome5 } from 'react-native-vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {validaToken} from '../Services/httpservices'
+import { validaToken } from '../Services/httpservices'
 
 
 const Cabecalho = () => {
     const navigation = useNavigation()
     let usuario
 
-    const redirecionadUsuario = async() => {
+    const redirecionadUsuario = async () => {
         try {
             usuario = JSON.parse(await AsyncStorage.getItem("@usuario"))
         } catch (error) {
             Alert.alert("Ocorreu um erro")
         }
-        
-        if (usuario) {
-            let statusToken
-            try {
-                statusToken = await validaToken(usuario.tokenjwt)
-            }
-            catch (error) {
-                
-            }
 
-            if (statusToken) {
-                navigation.navigate('Perfil')
-            }
-            else{
-                Alert.alert("Sessão inspirada", "Por favor faça o login novamente.")
-                navigation.navigate('Login')
-            }
-          }
-        else{
+        if (usuario) {
+            //     let statusToken
+            //     try {
+            //         statusToken = await validaToken(usuario.tokenjwt)
+            //     }
+            //     catch (error) {
+
+            //     }
+
+            //     if (statusToken) {
+            navigation.navigate('Perfil')
+            
+            //     }
+            //     else{
+            //         Alert.alert("Sessão inspirada", "Por favor faça o login novamente.")
+            //         navigation.navigate('Login')
+            //     }
+        }
+        else {
             navigation.navigate('Login')
         }
     }
 
     return (
-        <SafeAreaView style={style.cabecalho}> 
+        <SafeAreaView style={style.cabecalho}>
             <TouchableOpacity onPress={redirecionadUsuario}>
                 <FontAwesome5 name="user" size={25} color="white" />
             </TouchableOpacity>
@@ -61,17 +62,17 @@ export default Cabecalho
 
 
 const style = StyleSheet.create({
-    cabecalho:{
-        backgroundColor: Cores.primary, 
-        height:40,
-        alignItems: "center", 
-        paddingRight: 15, 
+    cabecalho: {
+        backgroundColor: Cores.primary,
+        height: 40,
+        alignItems: "center",
+        paddingRight: 15,
         paddingLeft: 15,
         flexDirection: 'row',
         borderBottomWidth: 2,
         justifyContent: 'space-between'
     },
-    logo:{
+    logo: {
         fontWeight: 'bold',
         fontSize: 24,
         color: 'white',
